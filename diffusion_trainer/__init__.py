44b314432673e6dce9453b5466569555be98475e
dvruette <dimitrivr@icloud.com>
2025-07-22 09:37:51 +0200

    add __init__ file

---
+++ diffusion_trainer/__init__.py
@@ -0,0 +1,23 @@
+from .diffusion_trainer import DiffusionTrainer
+from .diffusion_config import DiffusionConfig
+from .loss import GiddLoss
+from .schedule import (
+    MixingRate,
+    MixingDistribution,
+    MixingSchedule,
+    LinearMixingRate,
+    HybridMixingDistribution,
+    GeneralMixingDistribution,
+)
+
+__all__ = [
+    "DiffusionTrainer",
+    "DiffusionConfig",
+    "GiddLoss",
+    "MixingRate",
+    "MixingDistribution",
+    "MixingSchedule",
+    "LinearMixingRate",
+    "HybridMixingDistribution",
+    "GeneralMixingDistribution",
+]
