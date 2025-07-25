1536cc5df79f5386fbbd46f072f89d9bbcfa3d1e
dvruette <dimitrivr@icloud.com>
2025-07-25 15:40:35 +0200

    update and fix hparams

--- main.py
+++ main.py
@@ -5,17 +5,8 @@ from pprint import pprint
 import ray
 from eformer.executor.ray import TpuAcceleratorConfig, execute
 
-from easydel.utils.helpers import get_logger
-
-from diffusion_trainer import DiffusionTrainer, DiffusionConfig
-
-import jax
-jax.config.update('jax_disable_jit', True)
-
 # Initialize Ray for distributed computing. This must be done once per application.
 # ray.init()
-
-logger = get_logger(__name__)
 
 SAVE_DIRECTORY = os.environ.get("SAVE_DIRECTORY", "outputs/diffusion_trainer")
 
@@ -28,20 +19,20 @@ WANDB_ENTITY = os.environ.get("WANDB_ENTITY", None)
 # --- Environment and TPU Configuration ---
 # These environment variables are passed to each Ray worker to ensure they have
 # access to necessary tokens and use efficient shared memory for caching.
-TPU_EXECUTION_ENV_VARS = {
-    "EASYDEL_AUTO": "1",  # Enables EasyDeL's automatic sharding configuration.
-    "HF_TOKEN": os.environ.get("HF_TOKEN_FOR_EASYDEL", ""),  # Hugging Face token.
-    "HF_DATASETS_CACHE": "/dev/shm/huggingface-dataset",  # RAM-disk for dataset cache.
-    "HF_HOME": "/dev/shm/huggingface",  # RAM-disk for model cache.
-    "HF_DATASETS_OFFLINE": "0",  # Allow online dataset access.
-    "WANDB_API_KEY": os.environ.get("WANDB_API_KEY_FOR_EASYDEL", ""),  # W&B API key.
-}
+# TPU_EXECUTION_ENV_VARS = {
+#     "EASYDEL_AUTO": "1",  # Enables EasyDeL's automatic sharding configuration.
+#     "HF_TOKEN": os.environ.get("HF_TOKEN_FOR_EASYDEL", ""),  # Hugging Face token.
+#     "HF_DATASETS_CACHE": "/dev/shm/huggingface-dataset",  # RAM-disk for dataset cache.
+#     "HF_HOME": "/dev/shm/huggingface",  # RAM-disk for model cache.
+#     "HF_DATASETS_OFFLINE": "0",  # Allow online dataset access.
+#     "WANDB_API_KEY": os.environ.get("WANDB_API_KEY_FOR_EASYDEL", ""),  # W&B API key.
+# }
 
 # Additional pip packages to install on each Ray worker environment.
 TPU_PIP_PACKAGES = []
 
 # Print the environment variables for verification.
-pprint(TPU_EXECUTION_ENV_VARS)
+# pprint(TPU_EXECUTION_ENV_VARS)
 
 # # Defines the TPU environment for Ray, specifying the accelerator type and worker setup.
 # tpu_config = TpuAcceleratorConfig(
@@ -69,10 +60,20 @@ def main():
     import jax
     from jax import numpy as jnp
     from transformers import AutoTokenizer
+    from datasets import load_dataset
+
+    from diffusion_trainer import DiffusionTrainer, DiffusionConfig
+    
+    # jax.config.update('jax_disable_jit', True)
+
+    logger = ed.utils.get_logger(__name__)
 
     # --- Basic Training Parameters ---
-    max_length = 128
-    total_batch_size = 1
+    max_length = 1024
+    total_batch_size = 8
+
+    hidden_size = 768
+    head_dim = 64
 
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_ID)
 
@@ -80,20 +81,22 @@ def main():
     model = ed.GiddForDiffusionLM(
         config=ed.GiddConfig(
             vocab_size=len(tokenizer),
-            hidden_size=256,
-            intermediate_size=1024,
-            num_hidden_layers=4,
-            num_attention_heads=4,
-            head_dim=64,
+            hidden_size=hidden_size,
+            intermediate_size=4*hidden_size,
+            num_hidden_layers=12,
+            num_attention_heads=hidden_size // head_dim,
+            head_dim=head_dim,
             max_position_embeddings=max_length,
             resid_scale=4.0,
             init_scale=0.4,
             emb_init_scale=0.1,
             head_init_scale=0.0,
             sharding_axis_dims=(1, jax.process_count(), 1, -1, 1),
+            partition_axis=ed.PartitionAxis(),
             gradient_checkpointing=ed.EasyDeLGradientCheckPointers.NOTHING_SAVEABLE,
-            attn_mechanism=ed.AttentionMechanisms.AUTO,
-            scan_layers=True,
+            attn_mechanism=ed.AttentionMechanisms.SDPA,
+            attn_dtype=jnp.bfloat16,
+            # scan_layers=True,
         ),
         dtype=jnp.bfloat16,
         param_dtype=jnp.bfloat16,
@@ -112,10 +115,12 @@ def main():
         # This is MANDATORY for streaming datasets. It tells the trainer how many
         # steps constitute one "epoch". Should be ~ (total_dataset_size // total_batch_size).
         per_epoch_training_steps=98_000_000,
-        learning_rate=2e-4,
-        learning_rate_end=7e-6,
+        learning_rate=0.75 / hidden_size**0.5,
+        learning_rate_end=0.05 / hidden_size**0.5,
         optimizer=ed.EasyDeLOptimizers.ADAMW,
         scheduler=ed.EasyDeLSchedulers.COSINE,
+        warmup_steps=2000,
+        weight_decay=0.02,
         save_directory=SAVE_DIRECTORY,
         save_steps=1_000,
         save_total_limit=0,
@@ -124,27 +129,37 @@ def main():
         report_steps=50,
         log_steps=10,
         progress_bar_type="json",
+        track_memory=True,
+        use_grain=False,
     )
 
-    pprint(jax.tree.map(lambda x: x.shape if hasattr(x, "shape") else x, model.parameters))
+    # pprint(jax.tree.map(lambda x: x.shape if hasattr(x, "shape") else x, model.parameters))
+
+    # with jax.profiler.trace("./jax-trace", create_perfetto_link=False):
 
     # --- Streaming Dataset Setup ---
-    informs = [
-        ed.TextDatasetInform(content_field="tokens", path="dvruette/nemotron-cc-65btok", split="train"),
-        # ed.TextDatasetInform( # sample of reading from bucket.
-        #     content_field="text",
-        #     data_files="gs://your-bucket/raw/dclm/a3b142c/**",
-        #     split="train",
-        #     path=ed.DatasetType.JSON,
-        # ),
-        # ed.TextDatasetInform(
-        #     content_field="content",
-        #     data_files="gs://your-bucket/raw/starcoderdata-720c8c/9fc30b5/**/*.parquet",
-        #     split="train",
-        # ),
-    ]
-    mixture = ed.DatasetMixture(batch_size=1, informs=informs)
-    train_dataset = ed.DataManager.create_dataset_from_mixture(mixture)
+    # informs = [
+    #     ed.TextDatasetInform(content_field="tokens", path="dvruette/nemotron-cc-65btok", split="train"),
+    #     # ed.TextDatasetInform( # sample of reading from bucket.
+    #     #     content_field="text",
+    #     #     data_files="gs://your-bucket/raw/dclm/a3b142c/**",
+    #     #     split="train",
+    #     #     path=ed.DatasetType.JSON,
+    #     # ),
+    #     # ed.TextDatasetInform(
+    #     #     content_field="content",
+    #     #     data_files="gs://your-bucket/raw/starcoderdata-720c8c/9fc30b5/**/*.parquet",
+    #     #     split="train",
+    #     # ),
+    # ]
+    # mixture = ed.DatasetMixture(batch_size=1, informs=informs)
+    # train_dataset = ed.DataManager.create_dataset_from_mixture(mixture)
+
+    train_dataset = load_dataset(
+        "dvruette/nemotron-cc-65btok",
+        split="train",
+        num_proc=32,
+    )
 
     # --- Trainer Setup and Execution ---
     trainer = DiffusionTrainer(
@@ -155,9 +170,10 @@ def main():
         eval_dataset=None,
     )
 
+    # trainer.memory_monitor.start_monitoring()
+
     logger.info("Starting training...")
     trainer.train()
-
 
 if __name__ == "__main__":
     out = main()
