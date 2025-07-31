ef4e9037e7003bf6a8e5b59f85bae354b9b6d365
dvruette <dimitrivr@icloud.com>
2025-07-31 17:06:52 +0200

    update default config

--- diffusion_trainer/diffusion_config.py
+++ diffusion_trainer/diffusion_config.py
@@ -19,10 +19,10 @@ class DiffusionConfig(TrainingArguments):
     # loss arguments
     beta_is_div: float = field(default=1.0, metadata={"help": "Weight of the IS-divergence loss"})
     # noise sampling
-    noise_p_independent: float = field(default=0.5, metadata={"help": "Probability that SNR is sampled independently for each token."})
+    noise_p_independent: float = field(default=0.0, metadata={"help": "Probability that SNR is sampled independently for each token."})
     noise_p_linear: float = field(default=0.0, metadata={"help": "Probability that SNR is sampled linearly increasing in token position."})
-    noise_mask_p_prompt: float = field(default=0.1, metadata={"help": "Probability that a sample has a noise-free prompt."})
-    noise_mask_p_infilling: float = field(default=0.1, metadata={"help": "Probability that a sample has noise-free tokens (chosen uniformly at random)."})
+    noise_mask_p_prompt: float = field(default=0.0, metadata={"help": "Probability that a sample has a noise-free prompt."})
+    noise_mask_p_infilling: float = field(default=0.0, metadata={"help": "Probability that a sample has noise-free tokens (chosen uniformly at random)."})
     noise_mask_max_cond_frac: float = field(default=1.0, metadata={"help": "Max. fraction of noise-free tokens for each conditioning type (prompt, infilling)."})
     # mixing schedule
     mixing_rate: str | MixingRate = field(default="linear", metadata={"help": "Mixing schedule rate, can be 'linear' or any instance of `MixingRate`."})
