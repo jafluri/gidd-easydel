ebc7365f0b4f323f0855db1c929da9bfb28d1ddd
dvruette <dimitrivr@icloud.com>
2025-08-29 16:34:38 +0200

    update save path, remove debug prints

--- gidd_easydel/train.py
+++ gidd_easydel/train.py
@@ -86,9 +86,7 @@ def wsd_lr_schedule(total_steps: int, base_lr: float, warmup_steps: int = 0, coo
 
 def get_latest_checkpoint(checkpoint_dir):
     save_path = ed.EasyPath(checkpoint_dir)
-    print(save_path)
     checkpoint_files = list(save_path.glob("run-*/config.json"))
-    print(checkpoint_files)
     assert len(checkpoint_files) > 0, f"No checkpoints found in {checkpoint_dir}"
 
     def get_mtime(path):
@@ -100,8 +98,6 @@ def get_latest_checkpoint(checkpoint_dir):
     checkpoint_files.sort(key=get_mtime)
 
     latest_ckpt = checkpoint_files[-1].parent
-    print(latest_ckpt)
-    print(latest_ckpt.stem())
     step = int(latest_ckpt.stem().split("-")[-1])
 
     return latest_ckpt, step
@@ -188,9 +184,9 @@ def train(args):
     else:
         args.save_directory = os.path.join(
             args.save_directory,
-            args.wandb_name,
             args.wandb_tags,
             datetime.now().strftime("%Y-%m-%d"),
+            args.wandb_name,
             datetime.now().strftime("%H-%M-%S"),
         )
         model = GiddForDiffusionLM(
